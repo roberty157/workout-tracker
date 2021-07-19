81e5c79e2449c3b6f2a44e92b04c86cd7dd04e09
Robert <robertyeam@gmail.com>
2021-07-19 14:36:26 -0700

    almost done

--- server.js
+++ server.js
@@ -26,11 +26,11 @@ mongoose.connect(
     }
   );
   
-/*
+
 app.get("/", (req,res)=>{
     res.sendFile(path.join(__dirname + "./public/index.html"));
 });
-*/
+
 app.get("/exercise", (req,res)=>{
     res.sendFile(path.join(__dirname + "/public/exercise.html"));
 });
