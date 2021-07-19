a6da5261c4d2fd8f835cd97d52dc94e2ebcfd301
Robert <robertyeam@gmail.com>
2021-07-19 14:20:22 -0700

    deploying

--- Develop/server.js
+++ Develop/server.js
@@ -16,7 +16,16 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true , useUnifiedTopology: true});
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/ExampleDB',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
+  
 /*
 app.get("/", (req,res)=>{
     res.sendFile(path.join(__dirname + "./public/index.html"));
